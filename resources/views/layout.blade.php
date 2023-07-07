d457057d2fcbd1b60e3b75066728dc90c030c6a1
kuderluka <luka.kuder@aspiria.com>
2023-07-07 20:47:17 +0200

    Layout view created

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center header">
+        <h1>Notes</h1>
+        <div>
+            <button class="btn btn-primary button">Button 1</button>
+            <button class="btn btn-primary button">Button 2</button>
+            <button class="btn btn-primary button">Button 3</button>
+        </div>
+    </div>
+    <br>
+
+    @yield('content')
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
