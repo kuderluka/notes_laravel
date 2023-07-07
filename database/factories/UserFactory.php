b0182202dc1786e2058c60776d3badde02c55b4a
kuderluka <luka.kuder@aspiria.com>
2023-07-07 20:48:20 +0200

    User factory now fits the model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,13 +18,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'id' =>fake()->unique()->uuid(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'image' => Str::random(15),
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
