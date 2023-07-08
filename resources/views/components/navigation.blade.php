a5999db87b6e7612e21d31747fb8ed610e67e802
kuderluka <luka.kuder@aspiria.com>
2023-07-08 21:32:28 +0200

    navigation component generalized

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -3,8 +3,8 @@
     <div class="d-flex justify-content-between align-items-center header">
         <h3>You are currently viewing {{$heading}}</h3>
         <div>
-            <a href="/" class="btn btn-primary button">Create new</a>
-            <a href="/categories" class="btn btn-primary button">Go back</a>
+            <a href="/{{$heading}}/create" class="btn btn-primary button">Create new</a>
+            <a href="/" class="btn btn-primary button">Go back</a>
         </div>
     </div>
     <br>
