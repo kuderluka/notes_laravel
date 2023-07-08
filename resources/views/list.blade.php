46b4c0d273e65543db954d33970aff4b26a87fc4
kuderluka <luka.kuder@aspiria.com>
2023-07-08 14:18:02 +0200

    Syntax fixed

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,20 +1,19 @@
 @extends('layout')
 
+
 @section('content')
-    <h1>{{$heading . ': '}}</h1>
+    <x-navigation :heading="$heading"></x-navigation>
 
     @if(count($entries) == 0)
         <p>No {{$heading}} found!</p>
     @else
-
-                <table class="table">
-                    <x-dynamic-component :component="'thead.' . $heading" />
-                    <tbody>
-                    @foreach($entries as $entry)
-                        <x-dynamic-component :component="$heading" :entry="$entry"/>
-                    @endforeach
-                    </tbody>
-                </table>
-
+        <table class="table">
+            <x-dynamic-component :component="'thead.' . $heading" />
+            <tbody>
+            @foreach($entries as $entry)
+                <x-dynamic-component :component="'trow.' . $heading" :entry="$entry"/>
+            @endforeach
+            </tbody>
+        </table>
     @endif
 @endsection
