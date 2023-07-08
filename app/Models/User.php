cbe7ca2a7c53d3059ddfe60c86647e93cbd711a1
kuderluka <luka.kuder@aspiria.com>
2023-07-08 21:31:57 +0200

    User model fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,47 +4,25 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
-class User extends Authenticatable
+
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     public $incrementing = false;
     public $keyType = 'string';
 
-
-    /**
-     * The attributes that are mass assignable.
-     *
-    */
     protected $fillable = [
-        'name',
+        'id',
+        'username',
         'email',
         'password',
+        'image',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-    */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-    */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
 }
 
 
