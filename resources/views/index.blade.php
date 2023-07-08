330b18f421ab50bad4ef170f4652ec3ebc2f8fc9
kuderluka <luka.kuder@aspiria.com>
2023-07-08 14:18:19 +0200

    Added index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <div>
+        <br>
+        <h3 class="text-center">Welcome, please navigate using buttons in the top right corner</h3>
+    </div>
+@endsection
