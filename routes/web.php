36165df5bee78720041333374ad4cc3a31d8cfa8
kuderluka <luka.kuder@aspiria.com>
2023-07-08 14:17:38 +0200

    Added routes for creating, deleting and updating data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\UserController;
 use App\Models\Category;
+use App\Models\Note;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -15,21 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('index'); });
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/notes', [NoteController::class, 'index']);
 
-Route::get('/categories', function () {
-    return view('list', [
-        'heading' => 'Categories',
-        'entries' => Category::all()
-    ]);
-});
+//Creates a new entry
+Route::get('/users/create', [UserController::class, 'create']);
+Route::get('/categories/create', [CategoryController::class, 'create']);
+Route::get('/notes/create', [NoteController::class, 'create']);
 
-Route::get('/users', function () {
-    return view('list', [
-        'heading' => 'Users',
-        'entries' => User::all()
-    ]);
-});
+Route::get('/users/{user}', [UserController::class, 'updateForm']);
+Route::get('/categories/{category}', [CategoryController::class, 'updateForm']);
+Route::get('/notes/{note}', [NoteController::class, 'updateForm']);
 
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
