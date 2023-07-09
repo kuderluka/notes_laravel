0d435f097125bea419c6aa3fffdf53dafe6ddc90
kuderluka <luka.kuder@aspiria.com>
2023-07-09 18:55:49 +0200

    Checkbox now accepts models

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,6 @@
 <?php
-    foreach($entries as $entry) { ?>
+    foreach($entries as $entry) {?>
         <label for="{{$name}}[]"> <?php echo $entry[$property]?> </label>
-        <input type="checkbox" name="{{$name}}[]" value="<?php echo $entry['id']?>" <?php echo (in_array($entry[$property], $checkedEntries) ? 'checked' : '')  ?>>
+        <input type="checkbox" name="{{$name}}[]" value="<?php echo $entry['id'] ?>" <?php foreach($checkedEntries as $checked){ echo ($entry->is($checked)) ? 'checked' : '';}?>>
         <br>
     <?php }
