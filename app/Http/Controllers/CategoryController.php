c586aa9fca683a3680a7c70739bf67ca70d46be5
kuderluka <luka.kuder@aspiria.com>
2023-07-09 18:57:16 +0200

    Categories can now be edited and deleted with correct relations

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller
 {
@@ -17,16 +20,54 @@ class CategoryController extends Controller
     public function create()
     {
         return view('edit', [
-            'heading' => 'categories'
+            'heading' => 'categories',
+            'entry' => [],
+            'editing' => FALSE
         ]);
     }
 
-    public function edit(Category $category): string
+    public function edit(Category $category)
     {
         return view('edit', [
             'heading' => 'categories',
-            'entry' => $category
+            'entry' => $category,
+            'editing' => TRUE
         ]);
+    }
+
+    public function update(Request $request)
+    {
+        $validated = $request->validate([
+            'id' => 'required',
+            'users' => 'required',
+            'title' => ['required', Rule::unique('categories', 'title')],
+            'color' => 'required'
+        ]);
+
+        unset($validated['users']);
+
+        //To ne dela ce uporabim: ??????
+        //$category = Category::where('id', $validated['id'])->update($validated);
+        Category::where('id', $validated['id'])->update($validated);
+        $category = Category::find($request->id);
+
+        $category->users()->sync($request->users);
+        return redirect('/categories')->with('message', 'Category updated successfully');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'users' => 'required',
+            'title' => ['required', Rule::unique('categories', 'title')],
+            'color' => 'required'
+        ]);
+        $validated['id'] = (string) Str::orderedUuid();
+
+
+        $category = Category::create($validated);
+        $category->users()->attach($request->users);
+        return redirect('/categories')->with('message', 'Category created successfully');
     }
 
     public function destroy(Category $category): string
