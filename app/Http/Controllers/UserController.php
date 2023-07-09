1d309cdd43a237541d09f653e137949b8c31ab65
kuderluka <luka.kuder@aspiria.com>
2023-07-09 14:21:14 +0200

    Controller now handles updating, creating and deleting data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Validation\Rule;
 use JetBrains\PhpStorm\NoReturn;
 use Illuminate\Support\Str;
@@ -21,11 +22,12 @@ class UserController extends Controller
     public function create()
     {
         return view('edit', [
-            'heading' => 'users'
+            'heading' => 'users',
+            'entry' => []
         ]);
     }
 
-    public function edit(User $user): string
+    public function edit(User $user)
     {
         return view('edit', [
             'heading' => 'users',
@@ -33,14 +35,41 @@ class UserController extends Controller
         ]);
     }
 
+    public function update(Request $request)
+    {
+        File::delete(public_path() . '/storage/' . $request->oldImage);
+
+        if($request->hasFile('image')) {
+            $request['image'] = 'submitted';
+        }
+
+        $validated = $request->validate([
+            'id' => 'required',
+            'username' => 'required',
+            'password' => 'required',
+            'email' => ['required', 'email'],
+            'image' => 'required'
+        ]);
+
+        $validated['image'] = $request->file('image')->store('images', 'public');
+        User::where('id', $validated['id'])->update($validated);
+        return redirect('/users')->with('message', 'User updated successfully');
+    }
+
     public function store(Request $request) {
+        if($request->hasFile('image')) {
+            $request['image'] = 'submitted';
+        }
+
         $validated = $request->validate([
             'username' => ['required', Rule::unique('users', 'username')],
             'password' => 'required',
-            'email' => ['required', 'email' , Rule::unique('users', 'email')]
+            'email' => ['required', 'email' , Rule::unique('users', 'email')],
+            'image' => 'required'
         ]);
 
-        $validated += ['id' => (string) Str::orderedUuid()];
+        $validated['image'] = $request->file('image')->store('images', 'public');
+        $validated['id'] = (string) Str::orderedUuid();
 
         User::create($validated);
         return redirect('/users')->with('message', 'User created successfully');
@@ -48,6 +77,7 @@ class UserController extends Controller
 
     public function destroy(User $user): string
     {
+        File::delete(public_path() . '/storage/' . $user->image);
         $user->delete();
         return redirect('/users')->with('message', 'User deleted successfully');
     }
