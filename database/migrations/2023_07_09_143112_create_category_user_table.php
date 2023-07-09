3a28bd7f0193ac83fbbbf4b28df72745c26cb4a2
kuderluka <luka.kuder@aspiria.com>
2023-07-09 17:23:06 +0200

    Category_users pivot table generated

---
+++ database/migrations/2023_07_09_143112_create_category_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id', 36);
+            $table->string('category_id', 36);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_user');
+    }
+};
