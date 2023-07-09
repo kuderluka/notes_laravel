43060b4baa7cdd1b15652d5983e30fc1a9e4b1fa
kuderluka <luka.kuder@aspiria.com>
2023-07-09 14:18:14 +0200

    Attribute passed

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
 
 @section('content')
-    <x-dynamic-component :component="'edit-forms.' . $heading" />
+    <x-dynamic-component :component="'edit-forms.' . $heading" :entry="$entry"/>
 @endsection
