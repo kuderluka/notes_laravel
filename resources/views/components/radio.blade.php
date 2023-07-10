c1eb1cc70896a74bf7ea86ea1e76e2791d15dd04
kuderluka <luka.kuder@aspiria.com>
2023-07-10 21:57:02 +0200

    Radio component created

---
+++ resources/views/components/radio.blade.php
@@ -0,0 +1,9 @@
+<?php
+    foreach($entries as $entry) { ?>
+        <input type="radio" name="{{$type}}" value="{{$entry['id']}}" {{($entry->is($checked)) ? 'checked' : ''}}>
+        <label for="{{$type}}"> {{$entry[$property]}} </label> <br>
+<?php   } ?>
+
+
+
+
