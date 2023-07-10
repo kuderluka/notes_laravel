689ff458370c27017514b26eb1b77454c3ea9b2b
kuderluka <luka.kuder@aspiria.com>
2023-07-10 21:56:17 +0200

    Syntax fixed

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,6 @@
 <?php
     foreach($entries as $entry) {?>
         <label for="{{$name}}[]"> <?php echo $entry[$property]?> </label>
-        <input type="checkbox" name="{{$name}}[]" value="<?php echo $entry['id'] ?>" <?php foreach($checkedEntries as $checked){ echo ($entry->is($checked)) ? 'checked' : '';}?>>
+        <input type="checkbox" name="{{$name}}[]" value="{{$entry['id']}}" <?php foreach($checkedEntries as $checked){ echo ($entry->is($checked)) ? 'checked' : '';}?>>
         <br>
     <?php }
