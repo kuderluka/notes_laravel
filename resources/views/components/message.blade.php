2117e2c8ef5aa1475dd5b00bfcef09c451b4cd74
kuderluka <luka.kuder@aspiria.com>
2023-07-19 18:22:11 +0200

    Removed alpinejs

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,8 +1,6 @@
 @if(session()->has('message'))
-    <div x-data="{show: true}" x-init="setTimeout(() => show = false , 3000)" x-show="true" class="fixed top-0">
-        <p>
-            {{session('message')}}
-        </p>
-    </div>
+    <p>
+        {{session('message')}}
+    </p>
 @endif
 
