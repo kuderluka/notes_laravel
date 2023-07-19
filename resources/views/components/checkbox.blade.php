b535ada3a7b6af87808145a89b5aa6a186df4954
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-07-19 13:45:53 +0200

    Simplified using @foreach

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,5 @@
-<?php
-    foreach($entries as $entry) {?>
+@foreach($entries as $entry)
         <label for="{{$name}}[]"> <?php echo $entry[$property]?> </label>
         <input type="checkbox" name="{{$name}}[]" value="{{$entry['id']}}" <?php foreach($checkedEntries as $checked){ echo ($entry->is($checked)) ? 'checked' : '';}?>>
         <br>
-    <?php }
+@endforeach
--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -1,9 +1,5 @@
-<?php
-    foreach($entries as $entry) { ?>
+@foreach($entries as $entry)
         <input type="radio" name="{{$type}}" value="{{$entry['id']}}" {{($entry->is($checked)) ? 'checked' : ''}}>
         <label for="{{$type}}"> {{$entry[$property]}} </label> <br>
-<?php   } ?>
-
-
-
+@endforeach
 
