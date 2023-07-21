323c4f2858bf997d55b9b0286ddd52608f5f91b3
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-07-21 18:29:16 +0200

    Updated view

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,5 +1,5 @@
 @foreach($entries as $entry)
         <label for="{{$name}}[]"> <?php echo $entry[$property]?> </label>
-        <input type="checkbox" name="{{$name}}[]" value="{{$entry['id']}}" <?php foreach($checkedEntries as $checked){ echo ($entry->is($checked)) ? 'checked' : '';}?>>
+        <input type="checkbox" name="{{$name}}[]" value="{{$entry['id']}}" <?php if ($checkedEntries != NULL) {foreach($checkedEntries as $checked){ echo ($entry->is($checked)) ? 'checked' : '';}}?>>
         <br>
 @endforeach
