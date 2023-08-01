dbf2be1667fb518feb040b30d950cfe6ff8a9f4b
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-08-01 13:49:34 +0200

    Added sortablelinks()

--- resources/views/components/thead/categories.blade.php
+++ resources/views/components/thead/categories.blade.php
@@ -1,7 +1,7 @@
 <thead>
 <tr>
-    <th>Users</th>
-    <th>Title</th>
-    <th>Color</th>
+    <th>@sortablelink('users', 'Users')</th>
+    <th>@sortablelink('title', 'Title')</th>
+    <th>@sortablelink('color', 'Color')</th>
 </tr>
 </thead>
