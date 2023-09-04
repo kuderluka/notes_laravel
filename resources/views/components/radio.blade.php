5c765f0ea953e5f6c46851f8b61f7374824c1851
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-09-04 14:30:21 +0200

    removed unnecessary enter

--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -2,4 +2,3 @@
         <input type="radio" name="{{$type}}" value="{{$entry['id']}}" {{($entry->is($checked)) ? 'checked' : ''}}>
         <label for="{{$type}}"> {{$entry[$property]}} </label> <br>
 @endforeach
-
