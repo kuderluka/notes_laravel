c18890a981b6b7d342cae47c7198d434c2ee561c
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-09-12 12:38:41 +0200

    Breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\NoteController;
-use App\Http\Controllers\UserController;
-use App\Models\Category;
-use App\Models\Note;
-use App\Models\User;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,32 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('index'); })->name('index');
-
-Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
-    Route::get('/', [UserController::class, 'index'])->name('index');
-    Route::get('create', [UserController::class, 'create'])->name('create');
-    Route::put('store', [UserController::class, 'update'])->name('store');
-    Route::post('store', [UserController::class, 'store'])->name('store');
-    Route::get('{user}/edit', [UserController::class, 'edit'])->name('edit');
-    Route::get('{user}', [UserController::class, 'show'])->name('show');
-    Route::delete('{user}', [UserController::class, 'destroy'])->name('destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
-    Route::get('/', [CategoryController::class, 'index'])->name('index');
-    Route::get('create', [CategoryController::class, 'create'])->name('create');
-    Route::put('store', [CategoryController::class, 'update'])->name('store');
-    Route::post('store', [CategoryController::class, 'store'])->name('store');
-    Route::get('{category}', [CategoryController::class, 'edit'])->name('edit');
-    Route::delete('{category}', [CategoryController::class, 'destroy'])->name('destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::group(['prefix' => 'notes', 'as' => 'notes.'], function () {
-    Route::get('/', [NoteController::class, 'index'])->name('index');
-    Route::get('create', [NoteController::class, 'create'])->name('create');
-    Route::put('store', [NoteController::class, 'update'])->name('store');
-    Route::post('store', [NoteController::class, 'store'])->name('store');
-    Route::get('{note}', [NoteController::class, 'edit'])->name('edit');
-    Route::delete('{note}', [NoteController::class, 'destroy'])->name('destroy');
-});
+require __DIR__.'/auth.php';
