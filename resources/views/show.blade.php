01578ddd5237860fab50a513d951e15d8484d373
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-09-12 20:19:57 +0200

    Users page now displays his data

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container">
     <div class="d-flex justify-content-between align-items-center header">
-        <h4>{{$entry->username}}'s page</h4>
+        <h4>{{$user->username}}'s page</h4>
 
         <div>
             <a href="{{route('category.create')}}" class="btn btn-primary button">New category</a>
@@ -14,21 +14,31 @@
 
     <div class="container">
         <h5>Your notes:</h5>
-            @if(count($entry->notes) != 0)
-                @foreach($entry->notes as $note)
-                    &emsp;{{$note['title']}} <br>
-                @endforeach
+            @if(count($user->notes) == 0)
+                <p>You don't have any notes</p>
             @else
-                <p>&emsp;This user has no notes</p>
+                <table class="table">
+                    <x-dynamic-component :component="'thead.notes'" />
+                    <tbody>
+                    @foreach($user->notes as $note)
+                        <x-dynamic-component :component="'trow.notes'" :entry="$note" :editable="TRUE"/>
+                    @endforeach
+                    </tbody>
+                </table>
             @endif
 
         <h5>Your categories:</h5>
-            @if(count($entry->categories) != 0)
-                @foreach($entry->categories as $category)
-                    &emsp;{{$category['title']}} <br>
-                @endforeach
+            @if(count($user->categories) == 0)
+                <p>You don't have any categories</p>
             @else
-                <p>&emsp;This user has no categories</p>
+                <table class="table">
+                    <x-dynamic-component :component="'thead.categories'" />
+                    <tbody>
+                    @foreach($user->categories as $category)
+                        <x-dynamic-component :component="'trow.categories'" :entry="$category"/>
+                    @endforeach
+                    </tbody>
+                </table>
             @endif
     </div>
 </div>
