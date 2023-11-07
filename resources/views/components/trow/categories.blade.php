37ff9556ea181052d934e8eb290bec6ce3957845
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-11-07 08:57:01 +0100

    Updated confirm message

--- resources/views/components/trow/categories.blade.php
+++ resources/views/components/trow/categories.blade.php
@@ -10,7 +10,7 @@
     <td>
         <a href="{{route('category.edit', ['category' => $entry])}}" class="btn btn-secondary">Edit</a>
 
-        <form method="POST" onsubmit="return confirm('Are you sure?');" action="{{route('category.destroy', ['category' => $entry])}}">
+        <form method="POST" onsubmit="return confirm('Are you sure you want to remove yourself from this category? That will delete all of your associated notes.');" action="{{route('category.destroy', ['category' => $entry])}}">
             @csrf
             @method('DELETE')
             <button class="btn btn-danger">Delete</button>
