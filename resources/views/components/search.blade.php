f06e6b11687623cbd68207e6e5cd6c9276a4def0
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-11-07 23:19:37 +0100

    Value now updates

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,6 +1,7 @@
-<form action="{{ route('public.data') }}">
+<form action="{{ route('public.data') }}" method="GET">
     <div>
-        <input type="text" name="search" placeholder="Enter the series of characters you want to search for here"/>
+        <input type="text" name="search" placeholder="Enter the series of characters you want to search for here" value="{{ request('search') }}">
         <button type="submit"> Search </button>
     </div>
 </form>
+
