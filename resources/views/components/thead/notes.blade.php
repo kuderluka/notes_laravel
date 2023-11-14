c0beed907f63c63f0a46a9fe8951b7b8e2b9de94
kuderl <126083729+kuderluka@users.noreply.github.com>
2023-11-14 21:43:28 +0100

    Now highlighted

--- resources/views/components/thead/notes.blade.php
+++ resources/views/components/thead/notes.blade.php
@@ -1,14 +1,14 @@
 <thead>
 <tr>
-    <th>@sortablelink('user_id', 'User')</th>
-    <th>@sortablelink('category_id', 'Category')</th>
-    <th>@sortablelink('title', 'Title')</th>
-    <th>@sortablelink('content', 'Content')</th>
-    <th>@sortablelink('priority', 'Priority')</th>
-    <th>@sortablelink('deadline', 'Deadline')</th>
-    <th>@sortablelink('tags', 'Tags')</th>
+    <th class="{{ request()->input('sort') === 'user_id' ? 'column_sorted' : '' }}">@sortablelink('user_id', 'User')</th>
+    <th class="{{ request()->input('sort') === 'category_id' ? 'column_sorted' : '' }}">@sortablelink('category_id', 'Category')</th>
+    <th class="{{ request()->input('sort') === 'title' ? 'column_sorted' : '' }}">@sortablelink('title', 'Title')</th>
+    <th class="{{ request()->input('sort') === 'content' ? 'column_sorted' : '' }}">@sortablelink('content', 'Content')</th>
+    <th class="{{ request()->input('sort') === 'priority' ? 'column_sorted' : '' }}">@sortablelink('priority', 'Priority')</th>
+    <th class="{{ request()->input('sort') === 'deadline' ? 'column_sorted' : '' }}">@sortablelink('deadline', 'Deadline')</th>
+    <th class="{{ request()->input('sort') === 'tags' ? 'column_sorted' : '' }}">@sortablelink('tags', 'Tags')</th>
     @if($public)
-        <th>@sortablelink('public', 'Public')</th>
+        <th class="{{ request()->input('sort') === 'public' ? 'column_sorted' : '' }}">@sortablelink('public', 'Public')</th>
     @endif
 </tr>
 </thead>
