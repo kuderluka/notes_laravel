fa07b04692ad4e4eca513e77852b1491d78cab29
kuderl <126083729+kuderluka@users.noreply.github.com>
2024-03-18 16:05:33 +0100

    Added events t components

---
+++ client/src/app/components/events/event-thead/event-thead.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notes-event-thead',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-thead.component.html',
+  styleUrl: './event-thead.component.css'
+})
+export class EventTheadComponent {
+
+}
---
+++ client/src/app/components/events/event-trow/event-trow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notes-event-trow',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-trow.component.html',
+  styleUrl: './event-trow.component.css'
+})
+export class EventTrowComponent {
+
+}
